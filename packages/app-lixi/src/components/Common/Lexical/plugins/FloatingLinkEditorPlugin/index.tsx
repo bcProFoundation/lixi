ba00baf3b1415c6943cfa3d0528e929ef38eaaef
JimmyK300 <134896739+JimmyK300@users.noreply.github.com>
2023-07-18 11:54:54 +0700

    Fix color issue with in the hyperlink section in create post

--- packages/app-lixi/src/components/Common/Lexical/plugins/FloatingLinkEditorPlugin/index.tsx
+++ packages/app-lixi/src/components/Common/Lexical/plugins/FloatingLinkEditorPlugin/index.tsx
@@ -32,6 +32,12 @@ import styled from 'styled-components';
 import { Button } from 'antd';
 import { CheckOutlined, EditOutlined } from '@ant-design/icons';
 
+//Minh
+import { useAppSelector } from '@store/hooks';
+import { getCurrentThemes } from '@store/settings/selectors';
+import { get } from 'lodash';
+import { current } from '@reduxjs/toolkit';
+
 const StyledFloating = styled.div`
   position: absolute;
   top: 0;
@@ -132,7 +138,11 @@ function FloatingLinkEditor({
 
       setFloatingElemPosition(rect, editorElem, anchorElem);
       setLastSelection(selection);
-    } else if (!activeElement || activeElement.className !== 'link-input') {
+    } else if (
+      !activeElement ||
+      activeElement.className !== 'link-input' ||
+      activeElement.className !== 'link-input-dark'
+    ) {
       if (rootElement !== null) {
         setFloatingElemPosition(null, editorElem, anchorElem);
       }
@@ -222,9 +232,13 @@ function FloatingLinkEditor({
     editorRef.current.style.display = 'none';
   };
 
+  const currentTheme = useAppSelector(getCurrentThemes);
+  const linkInput = currentTheme ? 'link-input-dark' : 'link-input';
+  const styledFloating = currentTheme ? 'StyledFloatingDark' : 'StyledFloating';
+
   return (
-    <StyledFloating ref={editorRef}>
-      <div className="link-input">
+    <StyledFloating className={styledFloating} ref={editorRef}>
+      <div className={linkInput}>
         <input
           ref={inputRef}
           onChange={event => {
