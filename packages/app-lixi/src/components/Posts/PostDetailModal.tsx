0b47bfb59c85d2733a17546c0d10ef9dd174bbe3
ericson <phamnguyentanh.97@gmail.com>
2023-07-19 13:47:30 +0700

    add avatar for comment

--- packages/app-lixi/src/components/Posts/PostDetailModal.tsx
+++ packages/app-lixi/src/components/Posts/PostDetailModal.tsx
@@ -790,7 +790,7 @@ export const PostDetailModal: React.FC<PostDetailProps> = ({ post, classStyle }:
           </CommentContainer>
           <CommentInputContainer className="comment-input-container">
             <div className="ava-ico-cmt" onClick={() => router.push(`/profile/${selectedAccount.address}`)}>
-              <AvatarUser name={selectedAccount?.name} isMarginRight={false} />
+              <AvatarUser icon={selectedAccount?.avatar} name={selectedAccount?.name} isMarginRight={false} />
             </div>
             <StyledCommentContainer className="comment-container">
               <Controller
