bbfd3ed26975288c35413d3f13c3f30fef11b8dd
ericson <phamnguyentanh.97@gmail.com>
2023-07-19 12:05:11 +0700

    polish ui

--- packages/app-lixi/src/components/Posts/PostDetailModal.tsx
+++ packages/app-lixi/src/components/Posts/PostDetailModal.tsx
@@ -127,6 +127,9 @@ const PostContentDetail = styled.div`
     a {
       cursor: pointer;
     }
+    div {
+      max-width: 100%;
+    }
   }
   .images-post {
     cursor: pointer;
@@ -680,14 +683,15 @@ export const PostDetailModal: React.FC<PostDetailProps> = ({ post, classStyle }:
     <>
       <Modal
         width={'50vw'}
-        className={`${classStyle} post-detail-custom-modal ${isMobile
-          ? openPost
-            ? 'animate__animated animate__faster animate__slideInRight'
-            : 'animate__animated animate__faster animate__slideOutRight'
-          : openPost
+        className={`${classStyle} post-detail-custom-modal ${
+          isMobile
+            ? openPost
+              ? 'animate__animated animate__faster animate__slideInRight'
+              : 'animate__animated animate__faster animate__slideOutRight'
+            : openPost
             ? 'animate__animated animate__faster animate__zoomIn'
             : 'animate__animated animate__faster animate__zoomOut'
-          }`}
+        }`}
         transitionName=""
         style={{ top: 30 }}
         open={true}
