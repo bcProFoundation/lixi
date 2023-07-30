07b1b395a18f7b60bd3dd21177215ef04b7541a4
khanhvinhnguyen <khanhvinh.nguyen.0628@gmail.com>
2023-07-31 01:16:15 +0700

    issue-547

--- packages/app-lixi/src/components/Common/AvatarUser.tsx
+++ packages/app-lixi/src/components/Common/AvatarUser.tsx
@@ -8,6 +8,12 @@ const StyledAvatar = styled(Avatar)`
   font-size: 18px;
   display: flex;
   align-items: center;
+  justify-content: center;
+
+  img {
+    width: 46px !important;
+    height: 46px !important;
+  }
 `;
 
 type AvatarUserProps = {
--- packages/app-lixi/src/components/NotificationPopup/index.tsx
+++ packages/app-lixi/src/components/NotificationPopup/index.tsx
@@ -1,7 +1,7 @@
 import { Comment } from '@ant-design/compatible';
 import { Account, NotificationDto as Notification } from '@bcpros/lixi-models';
 import { AvatarUser } from '@components/Common/AvatarUser';
-import { getSelectedAccount } from '@store/account/selectors';
+import { getSelectedAccountId } from '@store/account/selectors';
 import { useAppDispatch, useAppSelector } from '@store/hooks';
 import { downloadExportedLixi } from '@store/lixi/actions';
 import {
@@ -176,7 +176,7 @@ const BlankNotification = styled.p`
 const NotificationPopup = (notifications: Notification[], account: Account, isPopover?: boolean) => {
   const dispatch = useAppDispatch();
   const router = useRouter();
-  const selectedAccount = useAppSelector(getSelectedAccount);
+  const selectedAccountId = useAppSelector(getSelectedAccountId);
 
   const handleDelete = (account: Account, notificationId: string) => {
     dispatch(deleteNotification({ mnemonichHash: account.mnemonicHash, notificationId }));
@@ -192,13 +192,10 @@ const NotificationPopup = (notifications: Notification[], account: Account, isPo
   };
 
   const handleReadAll = () => {
-    dispatch(readAllNotifications());
-    dispatch(
-      fetchNotifications({
-        accountId: selectedAccount.id,
-        mnemonichHash: selectedAccount.mnemonicHash
-      })
-    );
+    dispatch(readAllNotifications({
+      accountId: selectedAccountId,
+      mnemonichHash: account.mnemonicHash
+    }));
   };
 
   const menuItems = [{ label: 'All', key: 'all' }];
@@ -237,7 +234,7 @@ const NotificationPopup = (notifications: Notification[], account: Account, isPo
             ) : (
               <>
                 <Button type="primary" className="no-border-btn" onClick={onClickMenu}>
-                  {intl.get('general.all')}
+                  All
                 </Button>
               </>
             )}
