be63e864076013d5436df4f3fe686b0145f18373
ericson <phamnguyentanh.97@gmail.com>
2023-08-01 14:19:51 +0700

    handle issue 561 & 566

--- packages/app-lixi/src/components/Pages/PageDetail.tsx
+++ packages/app-lixi/src/components/Pages/PageDetail.tsx
@@ -60,6 +60,7 @@ import { useRepostMutation } from '@store/post/posts.api';
 import _ from 'lodash';
 import { getSelectedPostId } from '@store/post/selectors';
 import { setSelectedPost } from '@store/post/actions';
+import { ReactSVG } from 'react-svg';
 
 export type PageItem = PageQuery['page'];
 
@@ -105,15 +106,23 @@ const ProfileCardHeader = styled.div`
   border-bottom: 0;
   border-top-left-radius: var(--border-radius-item);
   border-top-right-radius: var(--border-radius-item);
-  .cover-img {
-    width: 100%;
-    height: 200px;
-    border-top-right-radius: var(--border-radius-item);
-    border-top-left-radius: var(--border-radius-item);
-    object-fit: cover;
-    @media (max-width: 768px) {
-      border-radius: 0;
+  .container-img {
+    position: relative;
+    .cover-img {
+      width: 100%;
       height: 200px;
+      border-top-right-radius: var(--border-radius-item);
+      border-top-left-radius: var(--border-radius-item);
+      object-fit: cover;
+      @media (max-width: 768px) {
+        border-radius: 0;
+        height: 200px;
+      }
+    }
+    button {
+      position: absolute;
+      bottom: 0.5rem;
+      right: 0.5rem;
     }
   }
   .info-profile {
@@ -173,10 +182,18 @@ const ProfileCardHeader = styled.div`
     }
     .action-profile {
       display: flex;
+      align-self: center;
       @media (max-width: 1001px) {
         text-align: center;
         button {
           margin-bottom: 4px;
+        }
+      }
+      button {
+        svg {
+          filter: var(--filter-color-primary);
+          width: 16px;
+          height: 16px;
         }
       }
     }
@@ -813,6 +830,10 @@ const PageDetail = ({ page, checkIsFollowed, isMobile }: PageDetailProps) => {
               src={urlPageCoverUpload || pageDetailData.cover || '/images/default-cover.jpg'}
               alt=""
             />
+            <Button type="primary" className="no-border-btn" onClick={() => uploadModal(false)}>
+              <CameraOutlined />
+              {intl.get('page.editCoverPhoto')}
+            </Button>
           </div>
           <div className="info-profile">
             <div className="wrapper-avatar">
@@ -841,14 +862,12 @@ const PageDetail = ({ page, checkIsFollowed, isMobile }: PageDetailProps) => {
                   style={{ marginRight: '1rem' }}
                   type="primary"
                   className="outline-btn"
+                  icon={
+                    <ReactSVG wrapper="span" className="anticon anticon-custom" src="/images/ico-edit-square.svg" />
+                  }
                   onClick={navigateEditPage}
                 >
-                  <EditOutlined />
                   {intl.get('page.editPage')}
-                </Button>
-                <Button type="primary" className="outline-btn" onClick={() => uploadModal(false)}>
-                  <CameraOutlined />
-                  {intl.get('page.editCoverPhoto')}
                 </Button>
               </div>
             )}
