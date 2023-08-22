fd4504d24cf64dbc0429e4941cd895ba739900dd
vince8x <vince.tran8x@gmail.com>
2023-08-22 14:50:13 +0700

    Add timeline model

---
+++ packages/lixi-models/src/lib/timeline/pagination/timeline-item-connection.model.ts
@@ -0,0 +1,6 @@
+import { ObjectType } from '@nestjs/graphql';
+import { Paginated } from '../../../core';
+import { TimelineItem } from '../timeline-item.model';
+
+@ObjectType()
+export class TimelineItemConnection extends Paginated(TimelineItem) { }
---
+++ packages/lixi-models/src/lib/timeline/timeline-item.model.ts
@@ -0,0 +1,20 @@
+import { Field, ID, ObjectType, createUnionType } from '@nestjs/graphql';
+import { Post } from '../post';
+
+export const TimelineItemData = createUnionType({
+  name: 'TimelineItemData',
+  types: () => [Post] as const,
+});
+
+@ObjectType()
+export class TimelineItem {
+  @Field(() => ID)
+  id: string
+
+  @Field(() => TimelineItemData, { nullable: true })
+  data?: typeof TimelineItemData;
+
+  constructor(partial: Partial<TimelineItem>) {
+    Object.assign(this, partial);
+  }
+}
