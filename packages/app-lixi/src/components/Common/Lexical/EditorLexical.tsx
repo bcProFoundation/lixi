a7b8f51b125ffb0901c76ef1fc5f2abf2d076b62
Vu Nguyen <hoangvu96z@gmail.com>
2023-09-06 17:13:02 +0700

    update as comment

--- packages/app-lixi/src/components/Common/Lexical/EditorLexical.tsx
+++ packages/app-lixi/src/components/Common/Lexical/EditorLexical.tsx
@@ -18,7 +18,7 @@ import AutoLinkPlugin from './plugins/AutoLinkPlugin';
 import AutoEmbedPlugin from './plugins/AutoEmbedPlugin';
 import { MultiUploader } from '../Uploader/MultiUploader';
 import { PictureOutlined, CloseOutlined } from '@ant-design/icons';
-import { UPLOAD_TYPES } from '@bcpros/lixi-models/constants';
+import { UPLOAD_API_S3_MULTIPLE, UPLOAD_TYPES } from '@bcpros/lixi-models/constants';
 import styled from 'styled-components';
 import LinkPlugin from './plugins/LinkPlugin';
 import ButtonLinkPlugin from './plugins/ButtonLinkPlugin';
@@ -32,6 +32,8 @@ import { removeUpload } from '@store/account';
 import useWindowDimensions from '@hooks/useWindowDimensions';
 import YouTubePlugin from './plugins/YouTubePlugin';
 import FigmaPlugin from './plugins/FigmaPlugin';
+import { showToast } from '../../../../../redux-store/src/store/toast/actions';
+import axiosClient from '../../../../../redux-store/src/utils/axiosClient';
 
 export type EditorLexicalProps = {
   initialContent?: string;
@@ -160,6 +162,8 @@ const EditorLexical = (props: EditorLexicalProps) => {
   const { width } = useWindowDimensions();
   const [isMobile, setIsMobile] = useState(false);
   const postCoverUploads = useAppSelector(getPostCoverUploads);
+  const inputText = useRef(null);
+  const multiUploader = useRef(null);
   const imagesList = postCoverUploads.map(img => {
     const imgUrl = `${process.env.NEXT_PUBLIC_CF_IMAGES_DELIVERY_URL}/${process.env.NEXT_PUBLIC_CF_ACCOUNT_HASH}/${img.cfImageId}/public`;
     let width = img?.width || 4;
@@ -179,13 +183,12 @@ const EditorLexical = (props: EditorLexicalProps) => {
   useEffect(() => {
     const isMobile = width < 960 ? true : false;
     setIsMobile(isMobile);
+    inputText.current?.addEventListener('paste', handlePasteImage);
+    return () => {
+      inputText.current?.removeEventListener('paste', handlePasteImage);
+    };
   }, [width]);
 
-  const onRef = (_floatingAnchorElem: HTMLDivElement) => {
-    if (_floatingAnchorElem !== null) {
-      setFloatingAnchorElem(_floatingAnchorElem);
-    }
-  };
 
   const Placeholder = () => {
     return <div className="EditorLexical_placeholder">{intl.get('general.createPost')}</div>;
@@ -208,6 +211,26 @@ const EditorLexical = (props: EditorLexicalProps) => {
       dispatch(removeUpload({ type: 'post', id: imgId }));
     }
   };
+
+  const handlePasteImage = evt => {
+
+    const clipboardItems = evt.clipboardData.items;
+    const items = [].slice.call(clipboardItems).filter(function (item) {
+      // Filter the image items only
+      return /^image\//.test(item.type);
+    });
+    if (items.length === 0) {
+      return;
+    }
+
+    const item = items[0];
+    const blob = item.getAsFile();
+
+    let file = new File([blob], "file name", { type: "image/jpeg", lastModified: new Date().getTime() }, 'utf-8');
+    console.log('hehe: ', file);
+
+    multiUploader.current?.uploadImageFromClipboard({ file: file });
+  }
 
   const imageRenderer = useCallback(({ photo }) => {
     return (
@@ -239,7 +262,7 @@ const EditorLexical = (props: EditorLexicalProps) => {
           <div className="EditorLexical_container">
             <PlainTextPlugin
               contentEditable={
-                <div className="editor" ref={onRef}>
+                <div className="editor" ref={inputText}>
                   <ContentEditable className="EditorLexical_root" />
                 </div>
               }
@@ -318,6 +341,7 @@ const EditorLexical = (props: EditorLexicalProps) => {
             </div>
             <div className="EditorLexical_action">
               <MultiUploader
+                ref={multiUploader}
                 type={UPLOAD_TYPES.POST}
                 isIcon={true}
                 icon={'/images/ico-picture.svg'}
