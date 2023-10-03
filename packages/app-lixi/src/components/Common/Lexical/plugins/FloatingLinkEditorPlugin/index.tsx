6d958d4ead47e8060e12f7d3a75c77ae1ac3f2fa
KenNguyen-dev <74177525+KenNguyen-dev@users.noreply.github.com>
2023-10-03 12:18:20 +0700

    Fix #748 (#785)

--- packages/app-lixi/src/components/Common/Lexical/plugins/FloatingLinkEditorPlugin/index.tsx
+++ packages/app-lixi/src/components/Common/Lexical/plugins/FloatingLinkEditorPlugin/index.tsx
@@ -1,16 +1,10 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-import { $isAutoLinkNode, $isLinkNode, TOGGLE_LINK_COMMAND, LinkNode } from '@lexical/link';
+import { $isAutoLinkNode, $isLinkNode, TOGGLE_LINK_COMMAND } from '@lexical/link';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { $findMatchingParent, mergeRegister } from '@lexical/utils';
 import {
   $getSelection,
   $isRangeSelection,
+  CLICK_COMMAND,
   COMMAND_PRIORITY_CRITICAL,
   COMMAND_PRIORITY_HIGH,
   COMMAND_PRIORITY_LOW,
@@ -26,53 +20,159 @@ import * as React from 'react';
 import { createPortal } from 'react-dom';
 
 import { getSelectedNode } from '../../utils/getSelectedNode';
-import { setFloatingElemPosition } from '../../utils/setFloatingElemPosition';
+import { setFloatingElemPositionForLinkEditor } from '../../utils/setFloatingElemPositionForLinkEditor';
 import { sanitizeUrl } from '../../utils/url';
 import styled from 'styled-components';
 import { Button } from 'antd';
-import { CheckOutlined, EditOutlined } from '@ant-design/icons';
 
-import { useAppSelector } from '@store/hooks';
-import { getCurrentThemes } from '@store/settings/selectors';
-import { get } from 'lodash';
-import { current } from '@reduxjs/toolkit';
-
-const StyledFloating = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 10;
-  max-width: 400px;
-  width: 100%;
-  opacity: 0;
-  background-color: #fff;
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
-  border-radius: 8px;
-  transition: opacity 0.5s;
-  will-change: transform;
-  padding: 8px;
-
-  .link-input {
+const StyledLinkEditor = styled.div`
+  .link-editor {
     display: flex;
-    justify-content: space-between;
-    background: var(--bg-color-light-theme);
-    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    max-width: 400px;
+    width: 100%;
+    opacity: 0;
+    background-color: #fff;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+    border-radius: 0 0 8px 8px;
+    transition: opacity 0.5s;
+    will-change: transform;
+  }
+
+  .link-editor .button {
+    width: 20px;
+    height: 20px;
+    display: inline-block;
+    padding: 6px;
     border-radius: 8px;
-    padding: 4px 8px;
+    cursor: pointer;
+    margin: 0 2px;
+  }
+
+  .link-editor .button.hovered {
+    width: 20px;
+    height: 20px;
+    display: inline-block;
+    background-color: #eee;
+  }
+
+  .link-editor .button i,
+  .actions i {
+    background-size: contain;
+    display: inline-block;
+    height: 20px;
+    width: 20px;
+    vertical-align: -0.25em;
+  }
+
+  .link-editor .link-view {
+    display: block;
+    width: calc(100% - 24px);
+    margin: 8px 12px;
+    padding: 8px 12px;
+    border-radius: 15px;
     font-size: 15px;
-    button {
-      span {
-        font-size: 22px;
-      }
-      &:hover {
-        span {
-          color: var(--color-primary);
-        }
-      }
-    }
-    input {
-      flex-grow: 1;
-    }
+    color: rgb(5, 5, 5);
+    border: 0;
+    outline: 0;
+    position: relative;
+    font-family: inherit;
+  }
+
+  .link-editor .link-view a {
+    display: block;
+    word-break: break-word;
+    width: calc(100% - 33px);
+  }
+
+  .link-editor .link-edit {
+    background-image: url(images/ico-edit-square.svg);
+    background-size: 16px;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 35px;
+    vertical-align: -0.25em;
+    position: absolute;
+    right: 30px;
+    top: 0;
+    bottom: 0;
+    cursor: pointer;
+  }
+
+  .link-editor .link-trash {
+    background-image: url(images/ico-trash.svg);
+    background-size: 16px;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 35px;
+    vertical-align: -0.25em;
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    cursor: pointer;
+  }
+
+  .link-editor .link-cancel {
+    background-image: url(images/x-icon.svg);
+    background-size: 16px;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 35px;
+    vertical-align: -0.25em;
+    margin-right: 2px;
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    cursor: pointer;
+  }
+
+  .link-editor .link-confirm {
+    background-image: url(images/ico-check-circle.svg);
+    background-size: 16px;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 35px;
+    vertical-align: -0.25em;
+    margin-right: 28px;
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    cursor: pointer;
+  }
+
+  .link-editor .link-input {
+    display: block;
+    width: calc(100% - 75px);
+    box-sizing: border-box;
+    margin: 12px 12px;
+    padding: 8px 12px;
+    border-radius: 15px;
+    background-color: #eee;
+    font-size: 15px;
+    color: rgb(5, 5, 5);
+    border: 0;
+    outline: 0;
+    position: relative;
+    font-family: inherit;
+  }
+
+  .link-editor .link-input a {
+    color: rgb(33, 111, 219);
+    text-decoration: underline;
+    white-space: nowrap;
+    overflow: hidden;
+    margin-right: 30px;
+    text-overflow: ellipsis;
+  }
+
+  .link-editor div.link-input a:hover {
+    text-decoration: underline;
   }
 `;
 
@@ -80,17 +180,21 @@ function FloatingLinkEditor({
   editor,
   isLink,
   setIsLink,
-  anchorElem
+  anchorElem,
+  isLinkEditMode,
+  setIsLinkEditMode
 }: {
   editor: LexicalEditor;
   isLink: boolean;
   setIsLink: Dispatch<boolean>;
   anchorElem: HTMLElement;
+  isLinkEditMode: boolean;
+  setIsLinkEditMode: Dispatch<boolean>;
 }): JSX.Element {
   const editorRef = useRef<HTMLDivElement | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
   const [linkUrl, setLinkUrl] = useState('');
-  const [isEditMode, setEditMode] = useState(false);
+  const [editedLinkUrl, setEditedLinkUrl] = useState('https://');
   const [lastSelection, setLastSelection] = useState<RangeSelection | GridSelection | NodeSelection | null>(null);
 
   const updateLinkEditor = useCallback(() => {
@@ -123,34 +227,23 @@ function FloatingLinkEditor({
       rootElement.contains(nativeSelection.anchorNode) &&
       editor.isEditable()
     ) {
-      const domRange = nativeSelection.getRangeAt(0);
-      let rect;
-      if (nativeSelection.anchorNode === rootElement) {
-        let inner = rootElement;
-        while (inner.firstElementChild != null) {
-          inner = inner.firstElementChild as HTMLElement;
-        }
-        rect = inner.getBoundingClientRect();
-      } else {
-        rect = domRange.getBoundingClientRect();
+      const domRect: DOMRect | undefined = nativeSelection.focusNode?.parentElement?.getBoundingClientRect();
+      if (domRect) {
+        domRect.y += 40;
+        setFloatingElemPositionForLinkEditor(domRect, editorElem, anchorElem);
       }
-
-      setFloatingElemPosition(rect, editorElem, anchorElem);
       setLastSelection(selection);
-    } else if (
-      !activeElement ||
-      (activeElement.className !== 'link-input' && activeElement.className !== 'link-input-dark')
-    ) {
+    } else if (!activeElement || activeElement.className !== 'link-input') {
       if (rootElement !== null) {
-        setFloatingElemPosition(null, editorElem, anchorElem);
+        setFloatingElemPositionForLinkEditor(null, editorElem, anchorElem);
       }
       setLastSelection(null);
-      setEditMode(false);
+      setIsLinkEditMode(false);
       setLinkUrl('');
     }
 
     return true;
-  }, [anchorElem, editor]);
+  }, [anchorElem, editor, setIsLinkEditMode]);
 
   useEffect(() => {
     const scrollerElem = anchorElem.parentElement;
@@ -213,104 +306,179 @@ function FloatingLinkEditor({
   }, [editor, updateLinkEditor]);
 
   useEffect(() => {
-    if (isEditMode && inputRef.current) {
+    if (isLinkEditMode && inputRef.current) {
       inputRef.current.focus();
     }
-  }, [isEditMode]);
+  }, [isLinkEditMode, isLink]);
 
-  const createNodeLink = (linkUrl: string, editor: LexicalEditor) => {
-    if (linkUrl !== '') {
-      editor.dispatchCommand(TOGGLE_LINK_COMMAND, sanitizeUrl(linkUrl));
-      editor.registerNodeTransform(LinkNode, linkNode => {
-        if (linkNode.getTarget() == null) {
-          linkNode.setTarget('_blank');
-        }
-      });
+  const monitorInputInteraction = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      handleLinkSubmission();
+    } else if (event.key === 'Escape') {
+      event.preventDefault();
+      setIsLinkEditMode(false);
     }
-    editorRef.current.style.display = 'none';
   };
 
-  const currentTheme = useAppSelector(getCurrentThemes);
-  const linkInputClassName = currentTheme === 'dark' ? 'link-input-dark' : 'link-input';
-  const styledFloatingClassName = currentTheme === 'dark' ? 'StyledFloatingDark' : 'StyledFloating';
+  const handleLinkSubmission = () => {
+    if (lastSelection !== null) {
+      if (linkUrl !== '') {
+        editor.dispatchCommand(TOGGLE_LINK_COMMAND, { url: sanitizeUrl(editedLinkUrl), target: '_blank' });
+      }
+      setEditedLinkUrl('https://');
+      setIsLinkEditMode(false);
+    }
+  };
 
   return (
-    <StyledFloating className={styledFloatingClassName} ref={editorRef}>
-      <div className={linkInputClassName}>
-        <input
-          ref={inputRef}
-          onChange={event => {
-            setLinkUrl(event.target.value);
-          }}
-          onKeyDown={event => {
-            if (event.key === 'Enter') {
-              event.preventDefault();
-              if (lastSelection !== null) {
-                createNodeLink(linkUrl, editor);
-              }
-            }
-          }}
-        />
-        <Button
-          type="text"
-          icon={<CheckOutlined />}
-          onMouseDown={event => event.preventDefault()}
-          onClick={() => {
-            if (lastSelection !== null) {
-              createNodeLink(linkUrl, editor);
-            }
-          }}
-        />
+    <StyledLinkEditor>
+      <div ref={editorRef} className="link-editor">
+        {!isLink ? null : isLinkEditMode ? (
+          <>
+            <input
+              ref={inputRef}
+              className="link-input"
+              value={editedLinkUrl}
+              onChange={event => {
+                setEditedLinkUrl(event.target.value);
+              }}
+              onKeyDown={event => {
+                monitorInputInteraction(event);
+              }}
+            />
+            <div
+              className="link-confirm"
+              role="button"
+              tabIndex={0}
+              onMouseDown={event => event.preventDefault()}
+              onClick={handleLinkSubmission}
+            />
+            <div
+              className="link-cancel"
+              role="button"
+              tabIndex={0}
+              onMouseDown={event => event.preventDefault()}
+              onClick={() => {
+                setIsLinkEditMode(false);
+              }}
+            />
+          </>
+        ) : (
+          <div className="link-view">
+            <a
+              href={sanitizeUrl(linkUrl) !== 'https://' ? sanitizeUrl(linkUrl) : null}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {linkUrl}
+            </a>
+            <div
+              className="link-edit"
+              role="button"
+              tabIndex={0}
+              onMouseDown={event => event.preventDefault()}
+              onClick={() => {
+                setEditedLinkUrl(linkUrl);
+                setIsLinkEditMode(true);
+              }}
+            />
+            <div
+              className="link-trash"
+              role="button"
+              tabIndex={0}
+              onMouseDown={event => event.preventDefault()}
+              onClick={() => {
+                editor.dispatchCommand(TOGGLE_LINK_COMMAND, null);
+              }}
+            />
+          </div>
+        )}
       </div>
-    </StyledFloating>
+    </StyledLinkEditor>
   );
 }
 
-function useFloatingLinkEditorToolbar(editor: LexicalEditor, anchorElem: HTMLElement): JSX.Element | null {
+function useFloatingLinkEditorToolbar(
+  editor: LexicalEditor,
+  anchorElem: HTMLElement,
+  isLinkEditMode: boolean,
+  setIsLinkEditMode: Dispatch<boolean>
+): JSX.Element | null {
   const [activeEditor, setActiveEditor] = useState(editor);
   const [isLink, setIsLink] = useState(false);
 
-  const updateToolbar = useCallback(() => {
-    const selection = $getSelection();
-    if ($isRangeSelection(selection)) {
-      const node = getSelectedNode(selection);
-      const linkParent = $findMatchingParent(node, $isLinkNode);
-      const autoLinkParent = $findMatchingParent(node, $isAutoLinkNode);
-
-      // We don't want this menu to open for auto links.
-      if (linkParent != null && autoLinkParent == null) {
-        setIsLink(true);
-      } else {
-        setIsLink(false);
+  useEffect(() => {
+    function updateToolbar() {
+      const selection = $getSelection();
+      if ($isRangeSelection(selection)) {
+        const node = getSelectedNode(selection);
+        const linkParent = $findMatchingParent(node, $isLinkNode);
+        const autoLinkParent = $findMatchingParent(node, $isAutoLinkNode);
+        // We don't want this menu to open for auto links.
+        if (linkParent !== null && autoLinkParent === null) {
+          setIsLink(true);
+        } else {
+          setIsLink(false);
+        }
       }
     }
-  }, []);
-
-  useEffect(() => {
-    return editor.registerCommand(
-      SELECTION_CHANGE_COMMAND,
-      (_payload, newEditor) => {
-        updateToolbar();
-        setActiveEditor(newEditor);
-        return false;
-      },
-      COMMAND_PRIORITY_CRITICAL
-    );
-  }, [editor, updateToolbar]);
-
-  return isLink
-    ? createPortal(
-        <FloatingLinkEditor editor={activeEditor} isLink={isLink} anchorElem={anchorElem} setIsLink={setIsLink} />,
-        anchorElem
+    return mergeRegister(
+      editor.registerUpdateListener(({ editorState }) => {
+        editorState.read(() => {
+          updateToolbar();
+        });
+      }),
+      editor.registerCommand(
+        SELECTION_CHANGE_COMMAND,
+        (_payload, newEditor) => {
+          updateToolbar();
+          setActiveEditor(newEditor);
+          return false;
+        },
+        COMMAND_PRIORITY_CRITICAL
+      ),
+      editor.registerCommand(
+        CLICK_COMMAND,
+        payload => {
+          const selection = $getSelection();
+          if ($isRangeSelection(selection)) {
+            const node = getSelectedNode(selection);
+            const linkNode = $findMatchingParent(node, $isLinkNode);
+            if ($isLinkNode(linkNode) && (payload.metaKey || payload.ctrlKey)) {
+              window.open(linkNode.getURL(), '_blank');
+              return true;
+            }
+          }
+          return false;
+        },
+        COMMAND_PRIORITY_LOW
       )
-    : null;
+    );
+  }, [editor]);
+
+  return createPortal(
+    <FloatingLinkEditor
+      editor={activeEditor}
+      isLink={isLink}
+      anchorElem={anchorElem}
+      setIsLink={setIsLink}
+      isLinkEditMode={isLinkEditMode}
+      setIsLinkEditMode={setIsLinkEditMode}
+    />,
+    anchorElem
+  );
 }
 
 export default function FloatingLinkEditorPlugin({
-  anchorElem = document.body
+  anchorElem = document.body,
+  isLinkEditMode,
+  setIsLinkEditMode
 }: {
   anchorElem?: HTMLElement;
+  isLinkEditMode: boolean;
+  setIsLinkEditMode: Dispatch<boolean>;
 }): JSX.Element | null {
   const [editor] = useLexicalComposerContext();
-  return useFloatingLinkEditorToolbar(editor, anchorElem);
+  return useFloatingLinkEditorToolbar(editor, anchorElem, isLinkEditMode, setIsLinkEditMode);
 }
