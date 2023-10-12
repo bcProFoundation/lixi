e3bfdd986dca25d8e30906d047fd78a854c3db0c
KenNguyen-dev <74177525+KenNguyen-dev@users.noreply.github.com>
2023-10-12 09:54:35 +0700

    fix #682 (#800)

--- packages/app-lixi/src/components/Common/CreatePostCard.tsx
+++ packages/app-lixi/src/components/Common/CreatePostCard.tsx
@@ -1,37 +1,16 @@
-import { DollarOutlined, GlobalOutlined, PlusCircleOutlined, ShopOutlined } from '@ant-design/icons';
+import { PlusCircleOutlined } from '@ant-design/icons';
 import { AuthorizationContext } from '@context/index';
-import { WalletContext } from '@context/walletProvider';
-import { CreatePostInput, OrderDirection, PageQueryItem, PostOrderField } from '@generated/index';
-import useXPI from '@hooks/useXPI';
-import { PatchCollection } from '@reduxjs/toolkit/dist/query/core/buildThunks';
-import { deleteEditorTextFromCache, removeAllUpload } from '@store/account/actions';
-import { getAccountInfoTemp, getEditorCache, getPostCoverUploads, getSelectedAccount } from '@store/account/selectors';
-import { closeActionSheet } from '@store/action-sheet/actions';
+import { PageQueryItem } from '@generated/index';
+import { getAccountInfoTemp, getSelectedAccount } from '@store/account/selectors';
 import { useAppDispatch, useAppSelector } from '@store/hooks';
-import { useCreatePostMutation } from '@store/post/posts.api';
 import { getShowCreatePost } from '@store/post/selectors';
-import { getLevelFilter } from '@store/settings';
-import {
-  getCurrentThemes,
-  getFilterPostsHome,
-  getFilterPostsPage,
-  getFilterPostsToken,
-  getIsTopPosts
-} from '@store/settings/selectors';
-import { api as timelineApi } from '@store/timeline/timeline.generated';
-import { showToast } from '@store/toast/actions';
-import { getAllWalletPaths, getSlpBalancesAndUtxos } from '@store/wallet';
-import { getUtxoWif } from '@utils/cashMethods';
-import { Button, Input, Modal, Space } from 'antd';
-import router from 'next/router';
-import React, { useContext, useEffect, useState } from 'react';
-import intl from 'react-intl-universal';
+import { Input, Space } from 'antd';
+import React, { useContext, useEffect } from 'react';
 import styled from 'styled-components';
+import { openModal } from '@store/modal';
 import useAuthorization from './Authorization/use-authorization.hooks';
 import AvatarUser from './AvatarUser';
 import { SocialsEnum } from './Embed';
-import EditorLexical from './Lexical/EditorLexical';
-import { currency } from './Ticker';
 
 type ErrorType = 'unsupported' | 'invalid';
 
@@ -108,61 +87,6 @@ const DesktopCreatePost = styled.div`
   }
 `;
 
-const UserCreate = styled.div`
-  .user-create-post {
-    display: flex;
-    gap: 1rem;
-    align-items: center;
-    margin-bottom: 24px;
-    img {
-      width: 48px;
-      height: 48px;
-    }
-    .user-info {
-      .title-user {
-        margin: 0;
-        font-weight: 500;
-        text-transform: capitalize;
-        line-height: 24px;
-        letter-spacing: 0.15px;
-        color: var(--text-color-on-background);
-      }
-      .location-fee {
-        display: flex;
-        align-items: baseline;
-      }
-      .btn-select {
-        background: var(--border-color-base);
-        border-radius: var(--border-radius-primary);
-        padding: 0 8px;
-        border: none;
-        margin-top: 4px;
-        span {
-          font-weight: 400;
-          font-size: 12px;
-          line-height: 20px;
-          letter-spacing: 0.25px;
-          color: #4e444b;
-          &.anticon {
-            font-size: 10px;
-          }
-        }
-      }
-      .post-fee {
-        font-weight: 400;
-        font-size: 12px;
-        line-height: 20px;
-        letter-spacing: 0.25px;
-        color: #4e444b;
-        padding-left: 5px;
-        &.anticon {
-          font-size: 10px;
-        }
-      }
-    }
-  }
-`;
-
 const SpaceIconNoneHover = styled(Space)`
   gap: 8px;
   padding: 6px;
@@ -211,37 +135,16 @@ const IconWImage = ({
 
 const CreatePostCard = (props: CreatePostCardProp) => {
   const dispatch = useAppDispatch();
-  const pathname = router.pathname ?? '';
-  const [enableEditor, setEnableEditor] = useState(false);
-  const postCoverUploads = useAppSelector(getPostCoverUploads);
   const { page, tokenPrimaryId, hashtagId, hashtags, query, autoEnable } = props;
-  const pageId = page ? page.id : undefined;
   const selectedAccount = useAppSelector(getSelectedAccount);
-  const editorCache = useAppSelector(getEditorCache);
-  const filterHome = useAppSelector(getFilterPostsHome);
-  const filterPage = useAppSelector(getFilterPostsPage);
-  const filterToken = useAppSelector(getFilterPostsToken);
-  const slpBalancesAndUtxos = useAppSelector(getSlpBalancesAndUtxos);
-  const walletPaths = useAppSelector(getAllWalletPaths);
-  const Wallet = React.useContext(WalletContext);
-  const currentTheme = useAppSelector(getCurrentThemes);
-  const { XPI, chronik } = Wallet;
-  const { sendXpi } = useXPI();
   const authorization = useContext(AuthorizationContext);
   const askAuthorization = useAuthorization();
   const showCreatePostMobile = useAppSelector(getShowCreatePost);
   const accountInfoTemp = useAppSelector(getAccountInfoTemp);
-  const isTop = useAppSelector(getIsTopPosts);
-  const level = useAppSelector(getLevelFilter);
-
-  const [
-    createPostTrigger,
-    { isLoading: isLoadingCreatePost, isSuccess: isSuccessCreatePost, isError: isErrorCreatePost }
-  ] = useCreatePostMutation();
 
   const handleNewPostClick = () => {
     if (authorization.authorized) {
-      setEnableEditor(!enableEditor);
+      dispatch(openModal('CreatePostModal', { page, hashtags, hashtagId, tokenPrimaryId, query, autoEnable }));
     } else {
       askAuthorization();
     }
@@ -249,137 +152,9 @@ const CreatePostCard = (props: CreatePostCardProp) => {
 
   useEffect(() => {
     if (autoEnable && authorization.authorized) {
-      setEnableEditor(true);
+      dispatch(openModal('CreatePostModal', { page, hashtags, hashtagId, tokenPrimaryId, query, autoEnable }));
     }
   }, []);
-
-  const handleCreateNewPost = async ({ htmlContent, pureContent }) => {
-    let timelinePatches: PatchCollection;
-    const params = {
-      orderBy: {
-        direction: OrderDirection.Desc,
-        field: PostOrderField.UpdatedAt
-      }
-    };
-
-    try {
-      let filterValue: number;
-      let createFeeHex;
-      if (pathname.includes('/token')) {
-        filterValue = filterToken;
-      } else if (pathname.includes('/page')) {
-        filterValue = filterPage;
-
-        try {
-          if (selectedAccount.id != page.pageAccountId && parseFloat(page.createPostFee) != 0) {
-            const fundingWif = getUtxoWif(slpBalancesAndUtxos.nonSlpUtxos[0], walletPaths);
-            createFeeHex = await sendXpi(
-              XPI,
-              chronik,
-              walletPaths,
-              slpBalancesAndUtxos.nonSlpUtxos,
-              currency.defaultFee,
-              '',
-              false, // indicate send mode is one to one
-              null,
-              page.pageAccount.address,
-              page.createPostFee,
-              true,
-              fundingWif,
-              true
-            );
-          }
-        } catch (error) {
-          throw new Error(intl.get('account.insufficientFunds'));
-        }
-      } else {
-        filterValue = filterHome;
-      }
-
-      const createPostInput: CreatePostInput = {
-        uploads: postCoverUploads.map(upload => upload.id),
-        htmlContent: htmlContent,
-        pureContent: pureContent,
-        pageId: pageId || undefined,
-        tokenPrimaryId: tokenPrimaryId || undefined,
-        createFeeHex: createFeeHex,
-        extraArguments: {
-          hashtagId: hashtagId,
-          hashtags: hashtags,
-          query: query,
-          isTop: String(isTop),
-          minBurnFilter: filterValue,
-          orderBy: {
-            direction: OrderDirection.Desc,
-            field: PostOrderField.UpdatedAt
-          }
-        }
-      };
-
-      await createPostTrigger({ input: createPostInput });
-
-      dispatch(
-        showToast('success', {
-          message: 'Success',
-          description: intl.get('post.createPostSuccessful'),
-          duration: 5
-        })
-      );
-
-      setEnableEditor(false);
-      dispatch(removeAllUpload());
-      dispatch(deleteEditorTextFromCache());
-    } catch (error) {
-      let message;
-      if (error.message === intl.get('account.insufficientFunds')) {
-        message = error.message;
-      } else {
-        message = intl.get('post.unableCreatePostServer');
-      }
-      if (timelinePatches) {
-        dispatch(timelineApi.util.patchQueryData('HomeTimeline', { level: level }, timelinePatches.inversePatches));
-      }
-      dispatch(
-        showToast('error', {
-          message: 'Error',
-          description: message,
-          duration: 5
-        })
-      );
-    }
-    if (autoEnable) {
-      dispatch(closeActionSheet());
-    }
-  };
-
-  const getCreatePostLocation = () => {
-    if (pathname.includes('/token')) {
-      return (
-        <React.Fragment>
-          {intl.get('post.token')} <DollarOutlined />
-        </React.Fragment>
-      );
-    } else if (pathname.includes('/page') || autoEnable) {
-      return (
-        <React.Fragment>
-          {intl.get('post.page')} <ShopOutlined />
-        </React.Fragment>
-      );
-    } else {
-      return (
-        <React.Fragment>
-          {intl.get('post.public')} <GlobalOutlined />
-        </React.Fragment>
-      );
-    }
-  };
-
-  const handleOnCancelCreatePost = () => {
-    setEnableEditor(false);
-    if (autoEnable) {
-      dispatch(closeActionSheet());
-    }
-  };
 
   return (
     <React.Fragment>
@@ -427,38 +202,6 @@ const CreatePostCard = (props: CreatePostCardProp) => {
           <img src="/images/ico-create-post.svg" alt="" />
         </div>
       </MobileCreatePost>
-
-      <WrapEditor>
-        <Modal
-          className={`${currentTheme === 'dark' ? 'ant-modal-dark' : ''} custom-modal-editor`}
-          transitionName=""
-          title="Create Post"
-          open={enableEditor}
-          footer={null}
-          maskClosable={false}
-          onCancel={handleOnCancelCreatePost}
-        >
-          <UserCreate>
-            <div className="user-create-post">
-              <AvatarUser icon={accountInfoTemp?.avatar} name={selectedAccount?.name} isMarginRight={false} />
-              <div className="user-info">
-                <p className="title-user">{selectedAccount?.name}</p>
-                <div className="location-fee">
-                  <Button className="btn-select">{getCreatePostLocation()}</Button>
-                  {page && page.createPostFee && selectedAccount?.id != page.pageAccountId && (
-                    <p className="post-fee">{`${intl.get('general.fee')} ${page.createPostFee} ${currency.ticker}`}</p>
-                  )}
-                </div>
-              </div>
-            </div>
-            <EditorLexical
-              onSubmit={value => handleCreateNewPost(value)}
-              loading={isLoadingCreatePost}
-              hashtags={hashtags}
-            />
-          </UserCreate>
-        </Modal>
-      </WrapEditor>
     </React.Fragment>
   );
 };
