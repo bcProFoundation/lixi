3d7a39ac115228b142d1b3b665873e94cfd7b324
Huygon <huygon764@gmail.com>
2023-12-18 23:43:18 +0700

    resolve comment

--- packages/app-lixi-api/src/modules/account/follow-score.loader.ts
+++ packages/app-lixi-api/src/modules/account/follow-score.loader.ts
@@ -6,6 +6,7 @@ import _ from 'lodash';
 import { RedisDataLoader } from 'src/common/redis/redis-dataloader';
 import { PrismaService } from '../prisma/prisma.service';
 import { FollowOfType } from '@bcpros/lixi-models';
+import { Prisma } from '@bcpros/lixi-prisma';
 
 @Injectable({ scope: Scope.REQUEST })
 export default class FollowScoreLoader {
@@ -17,77 +18,67 @@ export default class FollowScoreLoader {
     new DataLoader(
       async (followOfType: readonly FollowOfType[]) => {
         const listFollowOfType = _.compact(followOfType);
+        let mapItem = new Map();
+        const listPageIds = listFollowOfType.filter(item => !_.isNil(item.pageId)).map(item => item.pageId);
+        const listTokenIds = listFollowOfType.filter(item => !_.isNil(item.tokenId)).map(item => item.tokenId);
+        const listAccountIds = listFollowOfType.filter(item => !_.isNil(item.accountId)).map(item => item.accountId);
 
-        return followOfType.map(async followType => {
-          const { pageId, tokenId, accountId } = followType;
+        if (listPageIds.length > 0) {
+          const listFollowScorePage: any[] = await this.prisma.$queryRaw`
+          SELECT
+            follow_page.page_id, SUM(account_dana.dana_given) + SUM(account_dana.dana_received) AS FollowScore
+          FROM account_dana
+          JOIN account
+            ON account_dana.account_id = account.id 
+          JOIN follow_page
+            ON account.id = follow_page.account_id
+          WHERE follow_page.page_id IN (${Prisma.join(listPageIds)})
+          GROUP BY follow_page.page_id 
+        `;
+          listFollowScorePage.map(item => {
+            mapItem.set(item.page_id, item.followscore);
+          });
+        }
 
-          if (pageId) {
-            const accountIdsFollowPage = await this.prisma.followPage.findMany({
-              where: { pageId },
-              select: { accountId: true }
-            });
-            const listAccountIdsFollowPage = accountIdsFollowPage.map(item => item.accountId);
+        if (listTokenIds.length > 0) {
+          const listFollowScoreToken: any[] = await this.prisma.$queryRaw`
+          SELECT
+            follow_page.token_id, SUM(account_dana.dana_given) + SUM(account_dana.dana_received) AS FollowScore
+          FROM account_dana
+          JOIN account
+            ON account_dana.account_id = account.id
+          JOIN follow_page
+            ON account.id = follow_page.account_id
+          WHERE follow_page.token_id IN (${Prisma.join(listTokenIds)})
+          GROUP BY follow_page.token_id 
+        `;
+          listFollowScoreToken.map(item => {
+            mapItem.set(item.token_id, item.followscore);
+          });
+        }
 
-            const followScore = await this.prisma.accountDana.aggregate({
-              where: {
-                accountId: { in: listAccountIdsFollowPage }
-              },
-              _sum: {
-                danaGiven: true,
-                danaReceived: true
-              }
-            });
+        if (listAccountIds.length > 0) {
+          const listFollowScoreAccount: any[] = await this.prisma.$queryRaw`
+          SELECT
+            follow_account.following_account_id, SUM(account_dana.dana_given) + SUM(account_dana.dana_received) AS FollowScore
+          FROM account_dana
+          JOIN account
+            ON account_dana.account_id = account.id
+          JOIN follow_account
+            ON account.id = follow_account.follower_account_id
+          WHERE follow_account.following_account_id IN (${Prisma.join(listAccountIds)})
+          GROUP BY follow_account.following_account_id
+        `;
+          listFollowScoreAccount.map(item => {
+            mapItem.set(item.following_account_id, item.followscore);
+          });
+        }
 
-            const danaReceived = followScore._sum.danaReceived ?? 0;
-            const danaGiven = followScore._sum.danaGiven ?? 0;
-            const totalFollowScore = danaReceived + danaGiven;
-
-            return totalFollowScore;
-          } else if (accountId) {
-            const accountIdsFollowAccount = await this.prisma.followAccount.findMany({
-              where: { followingAccountId: accountId },
-              select: { followerAccountId: true }
-            });
-            const listAccountIdsFollowAccount = accountIdsFollowAccount.map(item => item.followerAccountId);
-
-            const followScore = await this.prisma.accountDana.aggregate({
-              where: {
-                accountId: { in: listAccountIdsFollowAccount }
-              },
-              _sum: {
-                danaGiven: true,
-                danaReceived: true
-              }
-            });
-
-            const danaReceived = followScore._sum.danaReceived ?? 0;
-            const danaGiven = followScore._sum.danaGiven ?? 0;
-            const totalFollowScore = danaReceived + danaGiven;
-
-            return totalFollowScore;
-          } else if (tokenId) {
-            const accountIdsFollowToken = await this.prisma.followPage.findMany({
-              where: { tokenId },
-              select: { accountId: true }
-            });
-            const listAccountIdsFollowToken = accountIdsFollowToken.map(item => item.accountId);
-
-            const followScore = await this.prisma.accountDana.aggregate({
-              where: {
-                accountId: { in: listAccountIdsFollowToken }
-              },
-              _sum: {
-                danaGiven: true,
-                danaReceived: true
-              }
-            });
-
-            const danaReceived = followScore._sum.danaReceived ?? 0;
-            const danaGiven = followScore._sum.danaGiven ?? 0;
-            const totalFollowScore = danaReceived + danaGiven;
-
-            return totalFollowScore;
-          }
+        return followOfType.map(item => {
+          const { accountId, pageId, tokenId } = item;
+          if (pageId) return mapItem.get(pageId) ?? 0;
+          if (tokenId) return mapItem.get(tokenId) ?? 0;
+          if (accountId) return mapItem.get(accountId) ?? 0;
         });
       },
       { cache: false }
@@ -96,7 +87,7 @@ export default class FollowScoreLoader {
       expire: 600,
       buffer: false,
       serialize: value => (value ? value.toString() : '0'),
-      deserialize: async value => _.toSafeInteger(value)
+      deserialize: value => _.toSafeInteger(value)
     }
   );
 }
