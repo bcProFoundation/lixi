5c397ab49cf08e73b9995c5b79f922e216ba7d7d
KenNguyen-dev <74177525+KenNguyen-dev@users.noreply.github.com>
2024-06-21 11:48:24 +0700

    Update escrow model (#970)

--- packages/lixi-models/src/lib/escrow/inputs/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './createOffer.input';
-export * from './createDispute.input';
-export * from './createEscrowOrder.input';
-export * from './offer-order.input';
-export * from './escrow-order-order.input';
-export * from './dispute-order.input';
