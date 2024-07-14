1d32bd0deeb51fba5257fe7731ae8efe9b7b3598
BoH <huygon764@gmail.com>
2024-07-15 01:53:33 +0700

    add index for ergon

--- packages/app-lixi-api/src/modules/dana/convert-dana.resolver.ts
+++ packages/app-lixi-api/src/modules/dana/convert-dana.resolver.ts
@@ -1,15 +1,13 @@
 import { Injectable, Logger, UseFilters, UseGuards } from '@nestjs/common';
-import { Args, Int, Resolver, Query } from '@nestjs/graphql';
+import { Args, Resolver, Query, Float } from '@nestjs/graphql';
 import { InjectRedis } from '@songkeys/nestjs-redis';
-import { ChronikClient } from 'chronik-client';
 import Redis from 'ioredis';
-import { InjectChronikClient } from 'nestjs-chronik';
 import { GqlHttpExceptionFilter } from 'src/middlewares/gql.exception.filter';
 import { GqlJwtAuthGuardByPass } from '../auth/guards/gql-jwtauth.guard';
 import { AccountEntity } from 'src/decorators';
 import { Account, COIN, ConvertDanaInput, DanaRate } from '@bcpros/lixi-models';
 import { template } from 'src/utils/stringTemplate';
-import { decode, encode } from '@msgpack/msgpack';
+import { decode } from '@msgpack/msgpack';
 import { KeyCurrentHeight } from './dana.constants';
 
 @Injectable()
@@ -21,7 +19,7 @@ export class ConvertDanaResolver {
 
   constructor(@InjectRedis() private readonly redis: Redis) {}
 
-  @Query(() => Number)
+  @Query(() => Float)
   @UseGuards(GqlJwtAuthGuardByPass)
   async convertDanaToCoin(
     @AccountEntity() account: Account,
@@ -37,7 +35,13 @@ export class ConvertDanaResolver {
     const danaRateBuff = await this.redis.hgetBuffer(keyHighestConvertRate, KeyCurrentHeight);
     if (!danaRateBuff) return 0;
     const danaRate = decode(danaRateBuff ?? '') as DanaRate;
-    const coinPerDana = Math.round(danaRate?.coinPerDana ?? 0);
+    let coinPerDana = Math.round(danaRate?.coinPerDana ?? 0);
+
+    if (convertToCoin === COIN.XRG) {
+      const xrgPerDana = danaRate?.coinPerDana ?? 0;
+      const mXrgPerDana = xrgPerDana * Math.pow(10, 6); // 1 Dana = 0.1mE
+      coinPerDana = parseFloat(mXrgPerDana.toFixed(1));
+    }
 
     return coinPerDana;
   }
