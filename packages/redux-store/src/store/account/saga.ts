705f38d0a85fb45eaf034e2cc9852b71f9913af9
Ken Nguyen <thekha2710@gmail.com>
2024-07-29 16:15:24 +0700

    add missing redux file

--- packages/redux-store/src/store/account/saga.ts
+++ packages/redux-store/src/store/account/saga.ts
@@ -98,8 +98,8 @@ const nameConfigGenerator: Config = {
  * Generate a account with random encryption password
  * @param action The data to needed generate a account
  */
-function* generateAccountSaga(action: PayloadAction<{ coin: COIN }>) {
-  const { coin } = action.payload;
+function* generateAccountSaga(action: PayloadAction<{ coin: COIN, telegramId: string }>) {
+  const { coin, telegramId } = action.payload;
   const { XPI } = callConfig.call.walletContext;
   const lang = 'english';
   const Bip39128BitMnemonic = XPI.Mnemonic.generate(128, XPI.Mnemonic.wordLists()[lang]);
@@ -118,7 +118,8 @@ function* generateAccountSaga(action: PayloadAction<{ coin: COIN }>) {
     encryptedMnemonic,
     mnemonicHash,
     language: locale,
-    rootCoin: coin ? coin : COIN.XPI
+    rootCoin: coin ? coin : COIN.XPI,
+    telegramId: telegramId || undefined
   };
 
   yield put(postAccount(account));
