56c71516c26db54ead7d8d475babbac789d9bb56
vince8x <vince.tran8x@gmail.com>
2024-08-20 14:23:49 +0700

    chore: update dependencies and improve CORS handling
      - Updated devDependencies in package.json:
        - husky from ^9.0.11 to ^9.1.4
        - syncpack from ^12.3.3 to ^12.4.0
        - removed trailing space from turbo version
    
      - Refactored CORS setup in main.ts:
        - Simplified CORS options to use a cleaner instance-based approach.
        - Enhanced environment variable checks for setting allowed origins.
    
      - Updated pnpm-lock.yaml to reflect changes in versions and addition of new

--- packages/app-lixi-api/src/main.ts
+++ packages/app-lixi-api/src/main.ts
@@ -34,12 +34,7 @@ async function bootstrap() {
   const fastifyAdapter = new FastifyAdapter({
     trustProxy: true
   });
-  fastifyAdapter
-    .getInstance()
-    .addContentTypeParser('application/json', { bodyLimit: 10048576 }, (_request, _payload, done) => {
-      done(null, (_payload as any).body);
-    });
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastifyAdapter, {
     logger: loggerConfig,
     snapshot: true
   });
@@ -58,29 +53,22 @@ async function bootstrap() {
 
   const allowedOrigins = _.compact(whitelistOrigins).map(origin => stripTrailingSlash(origin));
 
-  const corsOptions: FastifyCorsOptions = {
-    credentials: true,
-    origin:
-      process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'local'
-        ? ['*']
-        : function (origin, callback) {
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.indexOf(stripTrailingSlash(origin)) === -1) {
-              const msg = `The CORS policy for this site does not allow access from the specified Origin. ${origin}`;
-              callback(new Error(msg), true);
-            } else {
-              callback(null, true);
-            }
-          },
-    exposedHeaders: ['Authorization'],
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Origin, Account-Secret, lang',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    preflightContinue: false,
-    optionsSuccessStatus: 200
-  };
+  app.enableCors(instance => {
+    return (req: any, callback: any) => {
+      const corsOptions: FastifyCorsOptions = {
+        credentials: true,
+        origin: process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'local' ? true : allowedOrigins,
+        exposedHeaders: ['Authorization'],
+        allowedHeaders:
+          'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Origin, Account-Secret, lang',
+        methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+        preflightContinue: false,
+        optionsSuccessStatus: 200
+      };
 
-  app.register(fastifyCors, corsOptions);
+      callback(null, corsOptions);
+    };
+  });
 
   // Prisma
   const prismaService: PrismaService = app.get(PrismaService);
