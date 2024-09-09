ca6224524d2b2e079dd21f9bd995648c3e082fc9
Ken Nguyen <thekha2710@gmail.com>
2024-09-09 13:10:37 +0700

    Fix wallet node saga

--- packages/redux-store/src/store/wallet/saga.ts
+++ packages/redux-store/src/store/wallet/saga.ts
@@ -8,25 +8,29 @@ import { WalletPathAddressInfo } from '@bcpros/lixi-models/lib/wallet/wallet.mod
 
 function* activateWalletSaga(action: PayloadAction<{ mnemonic: string; coin: COIN }>) {
   try {
-    const Wallet = callConfig.call.walletContext;
+    let Wallet;
     const { mnemonic, coin } = action.payload;
     let walletPaths: WalletPathAddressInfo[];
     let defaultPath: string;
 
     switch (coin) {
       case COIN.XPI:
+        Wallet = callConfig.call.walletContext;
         defaultPath = "m/44'/10605'/0'/0/0";
         walletPaths = yield call(Wallet.getWalletPathDetails, mnemonic, [defaultPath]);
         break;
       case COIN.XEC:
+        Wallet = callConfig.call.walletContextNode;
         defaultPath = "m/44'/1899'/0'/0/0";
         walletPaths = yield call(Wallet.getWalletPathDetails, mnemonic, [defaultPath]);
         break;
       case COIN.XRG:
+        Wallet = callConfig.call.walletContext;
         defaultPath = "m/44'/2137'/0'/0/0";
         walletPaths = yield call(Wallet.getWalletPathDetails, mnemonic, [defaultPath]);
         break;
       default:
+        Wallet = callConfig.call.walletContext;
         defaultPath = "m/44'/10605'/0'/0/0";
         walletPaths = yield call(Wallet.getWalletPathDetails, mnemonic, [defaultPath]);
         break;
