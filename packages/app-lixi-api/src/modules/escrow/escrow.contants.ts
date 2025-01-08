75145bec2d6514ea52ee1c72cec54a8a18d84666
BoH <huygon764@gmail.com>
2025-01-08 12:10:00 +0700

    fix notif of bot (localEcash)

--- packages/app-lixi-api/src/modules/escrow/escrow.contants.ts
+++ packages/app-lixi-api/src/modules/escrow/escrow.contants.ts
@@ -5,3 +5,5 @@ export const TIMELINE_ESCROW_ORDER = {
   active: `${EscrowOrderStatus.ACTIVE}-${EscrowOrderStatus.PENDING}-${EscrowOrderStatus.ESCROW}`,
   unactive: `${EscrowOrderStatus.CANCEL}-${EscrowOrderStatus.COMPLETE}`
 };
+
+export const COIN_OTHERS = 'Others';
